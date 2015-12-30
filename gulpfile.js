6fc9a18f621c88fc5e4c051cf3ea9a84a5d5722a
joyfeel <joybee210@gmail.com>
2015-12-31 00:04:56 +0800

    Add simple email & password textbox

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const jspm = require('jspm');
+const gulp = require('gulp');
+
+// JSPM-cli api [https://github.com/jspm/jspm-cli/blob/master/docs/api.md]
+
+const paths = {
+	scripts: 'public/javascripts/**',
+  	main: 'public/javascripts/main',
+  	dest: 'public/dest/bundle.js'
+};
+
+jspm.setPackagePath('.');
+
+gulp.task('scripts', () => {
+    return jspm
+		.bundleSFX(paths.main, paths.dest, 
+			{ 
+				minify: false,
+				mangle: false,
+				lowResSourceMaps: false,
+				sourceMaps: true
+			})
+		.then(() => {
+			//console.log('Finished')
+		});
+});
+
+gulp.task('watch', () => {
+	gulp.watch([paths.scripts], ['scripts']).on('change', (event) => {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  		return;
+	});
+});
+
+gulp.task('default', ['watch', 'scripts']);
